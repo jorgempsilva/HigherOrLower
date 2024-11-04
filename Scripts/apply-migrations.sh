3119373481af7a41802978557e4d407aeba24109
jorgempsilva <jorgempsilva97@gmail.com>
2024-11-04 22:15:50 +0000

    chore: adjust ports, dockerfile and script

--- Scripts/apply-migrations.sh
+++ Scripts/apply-migrations.sh
@@ -5,8 +5,5 @@ echo "Starting migrations..."
 cd /app/HigherOrLower || { echo "HigherOrLower directory not found"; exit 1; }
 echo "Current directory: $(pwd)"
 echo "Running migrations..."
-
-dotnet ef database update --project /app/Infrastructure/ --startup-project /app/HigherOrLower/ --verbose || { echo "Migrations failed"; exit 1; }
-
-echo "Building the project..."
-dotnet build || { echo "Build failed. Check the errors above."; exit 1; }
+dotnet ef database update --project /app/Infrastructure/ --startup-project /app/HigherOrLower/ --context SqlContext --verbose || { echo "Migrations failed"; exit 1; }
+echo "Migrations applied successfully."
